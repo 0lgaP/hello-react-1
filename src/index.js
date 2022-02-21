5ed8563426e79ffff54ce1c39500f6a04d23c532
0lgatron <perronolga@gmail.com>
2022-02-21 13:53:18 -0500

    finished project

--- src/index.js
+++ src/index.js
@@ -3,22 +3,42 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 const Button = (props) => {
-  // your code here
+  const{word, reset} = props;
+  return (
+    // <input 
+    // value={word}
+    // onChange={(event) => setWord(event.target.value)}
+    // placeholder="reset"
+    // />
+    <button onClick={reset}>
+      {word}
+    </button>
+  );
 };
 
 const Application = () => {
+  
+  const [name, setName] = useState("");
+
+
 
   // your code here
 
   const reset = () => {
+    setName("");
     console.log("reset");
     // your code here
   };
-
+  const emptyCheck = (name === "") ? "" : `Hello ${name}`;
   return (
     <main>
-      {/* your code here -- this entire line including the curly braces can be removed */}
-      <h1>Hello React</h1>
+      <input 
+      value={name}
+      onChange={(event) => setName(event.target.value)}
+      placeholder="Type your name"
+      />
+      <Button word="Reset" reset={reset} />
+      <h1>{emptyCheck}</h1>
     </main>
   );
 };
